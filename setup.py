4dbbc20487328b1b6a19a2925fc80152734ccd3c
Александр Фролов <milki.frol@yandex.ru>
2025-04-15 21:31:55 +0300

    improved setup process to better work with windows

--- setup.py
+++ setup.py
@@ -4,23 +4,31 @@ from setuptools import find_packages, setup
 
 def main():
     package_name = 'mlpt'
-    packages = find_packages(where=package_name)
-    # Если хотим установить пакеты так, чтобы при импорте использовался просто "import <subpackage>"
+    # Ищем пакеты в каталоге mlpt
+    # Если не находится ultralytics, нужно ввести в терминале pip install -e mlpt/modules/ultralytics
+    packages = find_packages(where="mlpt", include=["mlpt.modules.ultralytics*"])
+    print("Найденные пакеты:", packages)
+
+
+    
+    # Чтение зависимостей из requirements.txt с указанием кодировки UTF-8
+    with open('requirements.txt', encoding='utf-8') as f:
+        reqs = [str(req) for req in parse_requirements(f)]
+    
     setup(
         name=package_name,
         version='0.0.1',
         author='HASKII',
         description='PPE-Detection package',
         package_dir={"": package_name},
-        packages=find_packages(where=package_name),
+        packages=packages,
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: Apache Software License",
             "Operating System :: OS Independent",
         ],
         python_requires='>=3.7',
-        install_requires=[str(req) for req in parse_requirements(
-            open('requirements.txt'))],
+        install_requires=reqs,
     )
 
 
